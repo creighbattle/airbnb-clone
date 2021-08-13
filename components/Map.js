64ee2ce54b620ca1457e3e73a433aa88e913067b
System Administrator <root@Creighs-MacBook-Air.local>
2021-08-13 06:44:58 -0400

    airbnb

---
+++ components/Map.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGl
+      mapStyle="mapbox://styles/creighbattle/cksa388mt0q2s17qegcg7mh3j"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewport) => setViewport(nextViewport)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              className="cursor-pointer text-2xl animate-bounce"
+              onClick={() => setSelectedLocation(result)}
+            >
+              📌
+            </p>
+          </Marker>
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGl>
+  );
+}
+
+export default Map;
---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiY3JlaWdoYmF0dGxlIiwiYSI6ImNrc2EzMnZ5ZTB5b3Iyc256b3JuNXZsMnAifQ.J1LcbAS1V__hSDLc4Wmw4A",
+  },
+};
